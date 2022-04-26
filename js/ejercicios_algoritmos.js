ffac3fc144a8a3d35c0108bad434b9019804b539
santiboss2309 <santiago.garcia.daza@alumnos.sip.cl>
2022-04-26 10:51:51 -0400

    hola

--- js/ejercicios_algoritmos.js
+++ js/ejercicios_algoritmos.js
@@ -47,3 +47,24 @@ for(var num1=0; num1<=3; num1++)
         console.log(msg1.length+msg2.length)
     }
   }
+
+  /*
+  4 veces 11
+  */
+
+  var array = [5,3,4,1]
+
+  for(var x=0;x<array.length;x++)
+  {
+      for(var y=array.length-1;y>=x;y--)
+      {
+          if(array[x]>array[y])
+          {
+              var temp=array[y]
+              array[y]=array[x]
+              array[x]=temp
+          }
+      }
+  }
+
+  console.log(array)
