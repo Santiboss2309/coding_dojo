0d78ae52c8436a57ff209cf48e64c309a9e043ae
santiboss2309 <santiago.garcia.daza@alumnos.sip.cl>
2022-04-26 10:37:27 -0400

    hola

--- js/ejercicios_algoritmos.js
+++ js/ejercicios_algoritmos.js
@@ -19,8 +19,31 @@ for(var num1=0; num1<=3; num1++)
 
  for(var num1=0; num1<=x.length-3; num1++)
  {
-     for(var num2=0; num2<=x.length-4; num2++)
+     for(var num2=0; num2<=y.length-4; num2++)
      {
          console.log(x+y)
      }
  }
+
+ /*
+  4 veces codingdojo
+ */
+
+  var msg1 = "hello"
+  var msg2 = "world"
+
+  for(var num1=1;num1<=msg1.length;num1++)
+  {
+    for(var num2=1;num2<=msg2.length;num2++)
+    {
+        if(msg1.length == 4)
+        {
+            console.log(msg1+''+msg2);
+        }
+    }
+
+    if(num1 !=4)
+    {
+        console.log(msg1.length+msg2.length)
+    }
+  }
