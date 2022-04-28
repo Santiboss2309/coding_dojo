a5f235aeded44cac8d81b553ad3490ea7c9acdbb
santiboss2309 <santiago.garcia.daza@alumnos.sip.cl>
2022-04-27 21:00:33 -0400

    hola

--- js/dojo.js
+++ js/dojo.js
@@ -1,10 +1,9 @@
 function abc(num) {
     var arr=[];
     for(var i=0; i<num; i++){
-        arr.push(i)
+        if(i%2 == 0) {
+            arr.push(i);
+        }
     }
     return arr;
 }
-
-dojo= abc(5);
-console.log(dojo);
